3362d5e1bcb5d3d9cab6d1b9a99ca213d766840c
Ye Chuah <yechuah@gmail.com>
2011-11-12 18:40:51 +1100

    remove Comment heading

--- sites/all/themes/abtpre_letschat_theme/templates/comment-wrapper.tpl.php
+++ sites/all/themes/abtpre_letschat_theme/templates/comment-wrapper.tpl.php
@@ -36,11 +36,6 @@
  */
 ?>
 <div id="comments" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <?php if ($content['comments'] && $node->type != 'forum'): ?>
-    <?php print render($title_prefix); ?>
-    <h2 class="title"><?php print t('Comments'); ?></h2>
-    <?php print render($title_suffix); ?>
-  <?php endif; ?>
 
   <?php print render($content['comments']); ?>
 
